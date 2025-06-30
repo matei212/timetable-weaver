7cae5f536e60a0720fa1b63021efbcb1b72cf0a0
Matei <mateicostan99@gmail.com>
2025-06-30 23:12:36 +0300

    Creează o componentă reutilizabilă pentru modale

---
+++ src/frontend/components/common/Modal.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren, useLayoutEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+type Props = {
+  isOpen?: boolean;
+  onClose?: () => void;
+  className?: string;
+} & PropsWithChildren;
+const Modal = ({ isOpen, children, onClose, className }: Props) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  useLayoutEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [isOpen]);
+
+  return createPortal(
+    <dialog
+      ref={dialogRef}
+      onClose={onClose}
+      className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform bg-transparent backdrop:bg-black/70 backdrop:backdrop-blur-sm ${className}`}
+    >
+      {children}
+    </dialog>,
+    document.body,
+  );
+};
+
+export default Modal;
